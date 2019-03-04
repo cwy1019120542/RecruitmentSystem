3d631890c141bfa244df95b593f6f9baef4b24b9
cwy1019120542 <1019120542@qq.com>
2019-03-04 17:37:29 +0800

    完成了求职者主页、搜索界面、职位详情界面的大部分后台代码

--- RecruitmentSystem/SuperY/admin.py
+++ RecruitmentSystem/SuperY/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from . import models
 admin.site.register(models.Applicant)
 admin.site.register(models.Company)
-# Register your models here.
+admin.site.register(models.ApplicantSearch)
+admin.site.register(models.Post)
+admin.site.register(models.PostTag)
--- RecruitmentSystem/SuperY/views.py
+++ RecruitmentSystem/SuperY/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render,redirect,reverse,HttpResponse
+from django.shortcuts import render,redirect,reverse,HttpResponse,get_object_or_404
 from . import models
 from django.db.models import Q
 import hashlib
+import demjson
+import random
+from itertools import chain
+from django.core.paginator import Paginator
 
 def md5_passwd(phone_number,passwd):
 	deal_text=phone_number[:5]+passwd+phone_number[6:]
@@ -19,15 +23,17 @@ def login_ajax(request):
 	identity=request.POST['identity']
 	same_user=eval(f'models.{identity.capitalize()}.objects.filter(Q(phone_number=number)|Q(mail=number))')
 	if not same_user:
-		return HttpResponse('该账号不存在')
+		return HttpResponse(demjson.encode({'error':'该账号不存在'}))
 	if '@' in number:
 		phone_number=eval(f'models.{identity.capitalize()}.objects.get(mail=number)')
 	else:
 		phone_number=number
 	passwd=md5_passwd(phone_number,passwd)
 	if passwd != same_user[0].passwd:
-		return HttpResponse('密码不正确')
-	return HttpResponse()
+		return HttpResponse(demjson.encode({'error':'密码不正确'}))
+	request.session['identity']=identity
+	request.session['phone_number']=phone_number
+	return HttpResponse(demjson.encode({'url':reverse(f'SuperY:{identity}_index',kwargs={'phone_number':phone_number})}))
 
 def register(request):
 	return render(request,'SuperY/register.html')
@@ -36,17 +42,73 @@ def register_ajax(request):
 	print(request.POST)
 	person_name=request.POST['person_name']
 	phone_number=request.POST['phone_number']
-	mail=request.POST['mail']
 	passwd=request.POST['passwd']
 	passwd=md5_passwd(phone_number,passwd)
-	print(passwd)
 	identity=request.POST['identity']
 	same_phone=eval(f'models.{identity.capitalize()}.objects.filter(phone_number=phone_number)')
 	if same_phone:
-		return HttpResponse('该手机号已被注册')
-	if mail:
-		same_mail=eval(f'models.{identity.capitalize()}.objects.filter(mail=mail)')
-		if same_mail:
-			return HttpResponse('该邮箱已被注册')
-	eval(f'models.{identity.capitalize()}.objects.create(person_name=person_name,phone_number=phone_number,mail=mail,passwd=passwd)')
+		return HttpResponse(demjson.encode({'error':'该手机号已被注册'}))
+	eval(f'models.{identity.capitalize()}.objects.create(person_name=person_name,phone_number=phone_number,passwd=passwd)')
 	return HttpResponse()
+
+def is_login(request):
+	identity=request.session.get('identity','')
+	phone_number=request.session.get('phone_number','')
+	if not identity or not phone_number:
+		return redirect(reverse('SuperY:login')) 
+
+def applicant_index(request,phone_number):
+	is_login(request)
+	applicant=models.Applicant.objects.get(phone_number=phone_number)
+	if request.method == 'POST':
+		search_word=request.POST['search_word']
+		if not search_word:
+			post_list=models.Post.objects.all()
+		else:
+			models.ApplicantSearch.objects.create(applicant=applicant,search_word=search_word)
+			post_list=models.Post.objects.filter(Q(company__company_name__icontains=search_word)|Q(post_name__icontains=search_word)|Q(work_place__icontains=search_word))
+		context_dict={'applicant':applicant,'post_list':post_list}
+		return render(request,'SuperY/applicant_search.html',context_dict)
+	applicant_search_list=models.ApplicantSearch.objects.filter(applicant=applicant)
+	if not applicant_search_list:
+		post_list=models.Post.objects.all()[:9]
+	else:
+		search_word=applicant_search_list[0].search_word
+		post_list=models.Post.objects.filter(Q(company__company_name__icontains=search_word)|Q(post_name__icontains=search_word)|Q(work_place__icontains=search_word))
+	if len(post_list) < 9:
+		supply_number=9-len(post_list)
+		post_list=chain(post_list,models.Post.objects.all()[:supply_number])
+	context_dict={'applicant':applicant,'post_list':post_list}
+	return render(request,'SuperY/applicant_index.html',context_dict)
+
+def applicant_search_ajax(request):
+	is_login(request)
+	search_word=request.POST['search_word']
+	url=reverse('SuperY:applicant_search',kwargs={'phone_number':request.session['phone_number'],'search_word':search_word,'page':1})
+	return HttpResponse(demjson.encode({'url':url}))
+
+def my_paginator(data_all,page_data_number,aim_page):
+	paginator=Paginator(data_all,page_data_number)
+	data_list=paginator.page(aim_page)
+	return paginator.count,paginator.num_pages,data_list
+
+
+def applicant_search(request,phone_number,search_word,page):
+	is_login(request)
+	applicant=models.Applicant.objects.get(phone_number=phone_number)
+	post_all=models.Post.objects.filter(Q(company__company_name__icontains=search_word)|Q(post_name__icontains=search_word)|Q(work_place__icontains=search_word))
+	if post_all:
+		models.ApplicantSearch.objects.create(applicant=applicant,search_word=search_word)
+	post_number,page_all,post_list=my_paginator(post_all,5,page)
+	context_dict={'applicant':applicant,'post_number':post_number,'page_all':page_all,'post_list':post_list}
+	return render(request,'SuperY/applicant_search.html',context_dict)
+
+def post_detail(request,phone_number,post_id):
+	is_login()
+	applicant=models.Applicant.objects.get(phone_number=phone_number)
+	post=models.Post.objects.get(id=post_id)
+	context_dict={'applicant':applicant,'post':post}
+	return render(request,'SuperY/post_detail',context_dict)
+
+def company_index(request,phone_number):
+	return render(request,'SuperY/company_index.html')
